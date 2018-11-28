c4a05b2405022cf82f9f3bca61e95a56b4402fcd
mookT <mook13168@hotmail.com>
2018-11-28 14:41:12 -0800

    arrow keys works now

--- Puzzle2048.java
+++ Puzzle2048.java
@@ -9,15 +9,34 @@ import java.awt.Font;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
-
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.util.Pair;
+	
 
 public class Puzzle2048 extends JFrame{
-	int[][] grid = new int[4][4];
+	final int size = 4;
 	JPanel panel = (JPanel) getContentPane();
-	JButton[][] board = new JButton[4][4];
+	JButton[][] board = new JButton[size][size];
+	int[][] grid = new int[size][size];
+	List<Pair <Integer, Integer>> empty = new ArrayList<Pair <Integer, Integer>>();
+	List<Pair <Integer, Integer>> notEmpty = new ArrayList<Pair <Integer, Integer>>();
+	
 	
 	public Puzzle2048(){
+		initAvailableGrid();
 		initUI();
+	}
+	
+	private void initAvailableGrid(){
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				empty.add(new Pair<Integer, Integer>(i,j));
+			}
+		}
 	}
 	
 	private void initUI(){
@@ -32,10 +51,10 @@ public class Puzzle2048 extends JFrame{
 		//initialize button
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setPreferredSize(new Dimension(500, 500));
-		buttonPanel.setLayout(new GridLayout(4,4));
+		buttonPanel.setLayout(new GridLayout(size,size));
 		buttonPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray,10));
-		for(int i = 0; i < 4; i++){
-			for(int j = 0; j < 4; j++){
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
 				board[i][j] = new JButton();
 				board[i][j].setEnabled(false);
 				board[i][j].setBorder(BorderFactory.createLineBorder(Color.darkGray,5));
@@ -45,6 +64,28 @@ public class Puzzle2048 extends JFrame{
 			}
 		}
 		
+		buttonPanel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int dir = e.getKeyCode();
+				if(dir == KeyEvent.VK_UP){
+					System.out.println("1");
+					addNext();
+				} else if(dir == KeyEvent.VK_DOWN){
+					System.out.println("2");
+					addNext();
+				} else if(dir == KeyEvent.VK_LEFT){
+					System.out.println("3");
+					addNext();
+				} else if(dir == KeyEvent.VK_RIGHT){
+					System.out.println("4");
+					addNext();
+				}
+            }
+        });
+		buttonPanel.setFocusable(true);
+		addNext();
+		
 		panel.add(title, BorderLayout.NORTH);
 		panel.add(buttonPanel, BorderLayout.SOUTH);
 		pack();
@@ -53,14 +94,47 @@ public class Puzzle2048 extends JFrame{
 		setVisible(true);
 	}
 	
-	private int nextNum(){
+	private int getNext(){
+		System.out.println("5");
 		Random rand = new Random();
-		int randNum = rand.nextInt(101);
-		return randNum;
+		int randNum = rand.nextInt(100);
+		int next;
+		if(randNum < 79){
+			next = 2;
+		}else{
+			next = 4;
+		}
+		return next;
+	}
+		
+	private int getRowCol(){
+		Random rand = new Random();
+		return rand.nextInt(empty.size());
+	}
+		
+	private void addNext(){
+		System.out.println("6");
+		System.out.println("available: " + empty.size());
+		if(empty.size() == 0){
+			return;
+		}
+		int coor = getRowCol();
+		Pair<Integer, Integer> p = empty.get(coor);
+		grid[p.getKey()][p.getValue()] = getNext();
+		empty.remove(coor);
+		notEmpty.add(p);		
+		updateGrid();
+	}
+	
+	private void updateGrid(){
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				board[i][j].setText(Integer.toString(grid[i][j]));
+			}
+		}
 	}
 	
 	public static void main(String[] args){
-		Puzzle2048 p = new Puzzle2048(); 
-		
+		Puzzle2048 game = new Puzzle2048();
 	}
  }
